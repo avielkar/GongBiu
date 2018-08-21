b7e2cf02bcf8d4b0b2338f3b4ba56237193fc804
avielkar <avraham.elkar@gmail.com>
2018-08-21 16:28:54 +0300

    Add test for test_parse_sentence_to_dict

--- tests/bag_of_words_test.py
+++ tests/bag_of_words_test.py
@@ -15,3 +15,12 @@ class BagOfWordsTests(unittest.TestCase):
 
         self.assertEqual(ret_words_count.tolist()[0], words_count)
         self.assertEqual(ret_words_list, words_list)
+
+    def test_parse_sentence_to_dict(self):
+        sentence = 'This test is unit test and unit is unit'
+        words_dict = {'and': 1, 'is': 2, 'test': 2, 'this': 1, 'unit': 3}
+
+        bag_of_words = BagOfWords()
+        expected_words_dict = bag_of_words.parse_sentence_to_dict(sentence)
+
+        self.assertEqual(words_dict, expected_words_dict)
