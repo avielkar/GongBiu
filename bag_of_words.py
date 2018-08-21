8a1ae92ca417cf35c7fb31576fbfc6f620c5394d
avielkar <avraham.elkar@gmail.com>
2018-08-21 13:34:53 +0300

    Move the file to it's correct folder..

---
+++ bag_of_words.py
@@ -0,0 +1,36 @@
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy
+
+class BagOfWords:
+
+    vectorizer = None #type: CountVectorizer
+
+    def __init__(self):
+        self.vectorizer = CountVectorizer()
+        pass
+
+    
+    def parse_sentence_to_tuple(self, sentence: string) -> tuple(list,numpy.array):
+        """
+        Returns the Bag of words of a sentence.
+        :param sentence: The sentence to count it's bag of words.
+        :return: The bag of words described by a tuple of words list and there matched array counters.
+        """
+        x = self.vectorizer.fit_transform(sentence)
+        return (self.vectorizer.get_feature_names() , x.toarray())
+
+    def parse_sentence_to_dict(self, sentence: string) -> dict[string, int]:
+        """
+        The bag of words described by a tuple of words list and there matched array counters.
+        :param sentence: The sentence to count it's bag of words.
+        :return: The bag of words described by a dictionary with keys as the word and values as the matched occurences of the word.
+        """
+        [words, words_counts] = self.parse_sentence_to_tuple(sentence)
+        bag_of_words_dict = dict()
+        index = 0
+        
+        for word in words:
+            bag_of_words_dict[word] = words_counts[index]
+            index += 1
+
+        return bag_of_words_dict
