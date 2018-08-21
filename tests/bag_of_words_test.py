a086c0ef5ad11b31ec0e4b1736cdc52ea0c51891
avielkar <avraham.elkar@gmail.com>
2018-08-21 16:13:17 +0300

    Fix the array to asarray.

--- tests/bag_of_words_test.py
+++ tests/bag_of_words_test.py
@@ -8,7 +8,7 @@ class BagOfWordsTests(unittest.TestCase):
     def test_parse_sentence_to_tuple(self):
         sentence = 'This test is unit test and unit is unit'
         words_list = ['and', 'is', 'test', 'this', 'unit']
-        words_count = numpy.array([1, 2, 2, 1, 3])
+        words_count = numpy.asarray([1, 2, 2, 1, 3])
 
         bag_of_words = BagOfWords()
         [ret_words_list, ret_words_count] = bag_of_words.parse_sentence_to_tuple(sentence)
