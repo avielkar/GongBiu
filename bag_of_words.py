06eccb49b22bc37f6bb49af2321c0b994572c969
avielkar <avraham.elkar@gmail.com>
2018-08-21 13:39:47 +0300

    Fix string to str in the bag_of_words class.

--- bag_of_words.py
+++ bag_of_words.py
@@ -1,25 +1,24 @@
 from sklearn.feature_extraction.text import CountVectorizer
 import numpy
 
-class BagOfWords:
 
-    vectorizer = None #type: CountVectorizer
+class BagOfWords:
+    vectorizer = None  # type: CountVectorizer
 
     def __init__(self):
         self.vectorizer = CountVectorizer()
         pass
 
-    
-    def parse_sentence_to_tuple(self, sentence: string) -> tuple(list,numpy.array):
+    def parse_sentence_to_tuple(self, sentence: str) -> tuple(list, numpy.array):
         """
         Returns the Bag of words of a sentence.
         :param sentence: The sentence to count it's bag of words.
         :return: The bag of words described by a tuple of words list and there matched array counters.
         """
         x = self.vectorizer.fit_transform(sentence)
-        return (self.vectorizer.get_feature_names() , x.toarray())
+        return (self.vectorizer.get_feature_names(), x.toarray())
 
-    def parse_sentence_to_dict(self, sentence: string) -> dict[string, int]:
+    def parse_sentence_to_dict(self, sentence: str) -> dict[str, int]:
         """
         The bag of words described by a tuple of words list and there matched array counters.
         :param sentence: The sentence to count it's bag of words.
@@ -28,7 +27,7 @@ class BagOfWords:
         [words, words_counts] = self.parse_sentence_to_tuple(sentence)
         bag_of_words_dict = dict()
         index = 0
-        
+
         for word in words:
             bag_of_words_dict[word] = words_counts[index]
             index += 1
